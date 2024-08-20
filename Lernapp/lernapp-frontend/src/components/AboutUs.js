54d2f4e0e8e7471f56f65b82189c4b52c5298e13
Luis Angermeier <luis-mathias-johannes.angermeier@iu-study.org>
2024-08-20 15:27:57 +0200

    20.08

---
+++ Lernapp/lernapp-frontend/src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>Welcome to Lernapp! We are dedicated to providing valuable learning resources and tools to help you succeed.</p>
+            <p>Our team is passionate about education and technology, and we are constantly working to improve our platform.</p>
+        </div>
+    );
+};
+
+export default AboutUs;
