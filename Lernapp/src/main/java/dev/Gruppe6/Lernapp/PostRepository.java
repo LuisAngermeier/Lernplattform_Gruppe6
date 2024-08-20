2d19414a4a0437c8b46b72573dd0a315a617640a
LuisAngermeier <91549269+LuisAngermeier@users.noreply.github.com>
2024-08-20 12:55:27 +0200

    Delete Lernapp directory

--- Lernapp/src/main/java/dev/Gruppe6/Lernapp/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.Gruppe6.Lernapp;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-    // Custom query methods can be added here if needed
-}
